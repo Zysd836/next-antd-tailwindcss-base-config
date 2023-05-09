afd0a6a0dd433c75ab1e76da9b0a00181af173ac
hoangnv <viethoangd836@gmail.com>
2023-05-09 16:08:05 +0700

    feat: add i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'vi'],
+  },
+  localePath: './locales',
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+}
